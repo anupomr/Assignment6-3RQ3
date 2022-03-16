aaa1957096503e5e0f451bb2ae6f749f8f1c5e70
anupomr <anupomr@yahoo.com>
2022-03-16 07:01:26 -0400

    Stub created

---
+++ PasswordManager.py
@@ -0,0 +1,31 @@
+
+class PasswordManager:
+    # Part A
+    def save_password (self, password, website):
+        return  None
+    def save_username(self, username, website):
+        return  None
+
+    # Part B
+    def retrive_login_username(self, website):
+        return None
+    def retrive_login_password(self, website):
+        return None
+    def insert_login_username(self, website):
+        return None
+    def insert_login_password(self, website):
+        return None
+
+    # Part C
+    def login_to_system(self, username, password):
+        return None
+    def loged_in_username(self):
+        return None
+
+    # Part D
+    def request_user_consent(self, user_consented):
+        return None
+    def retrive_user_consent(self):
+        return None
+
+
